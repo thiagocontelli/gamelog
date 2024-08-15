b76f4d5e2e52cfd2db3f7af0db056551f1f11c37
Thiago Contelli <thiagocontellid@gmail.com>
2024-08-15 12:04:23 -0300

    layout novo do game card

--- components/GameCard.tsx
+++ components/GameCard.tsx
@@ -9,24 +9,23 @@ export function GameCard({ game }: Props) {
   return (
     <View
       style={{
-        flexDirection: 'row',
-        padding: 8,
-        borderColor: 'black',
-        borderWidth: 1,
-        gap: 8,
-        backgroundColor: 'white'
+        borderRadius: 4,
+        backgroundColor: 'white',
+        overflow: 'hidden',
+        borderColor: 'lightgray',
+        borderWidth: 1
       }}
     >
-      <Image style={{ backgroundColor: 'lightgray', minHeight: 150 }} source={{ uri: game.poster_url }} width={100} />
-      <View style={{ flex: 1 }}>
+      <Image style={{ backgroundColor: 'lightgray', minHeight: 150, flex: 1 }} source={{ uri: game.poster_url }} />
+      <View style={{ flex: 1, padding: 8, borderTopWidth: 1, borderColor: 'lightgray' }}>
         <Text style={{ fontSize: 24, fontWeight: '700' }}>{game.name}</Text>
-        <Text style={{ fontSize: 16 }}>
+        <Text style={{ fontSize: 16, color: 'gray' }}>
           Finalizado em {new Date(game.finished_at).toLocaleDateString('pt-BR', { day: '2-digit', month: 'short', year: 'numeric' })}
         </Text>
 
-        <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginTop: 'auto' }}>
-          <Text style={{ fontSize: 20, textTransform: 'uppercase' }}>{game.platform}</Text>
-          <Text style={{ fontSize: 20 }}>Nota {game.rating}</Text>
+        <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginTop: 24 }}>
+          <Text style={{ fontSize: 18, textTransform: 'uppercase' }}>{game.platform}</Text>
+          <Text style={{ fontSize: 18 }}>Nota {game.rating}</Text>
         </View>
       </View>
     </View>
